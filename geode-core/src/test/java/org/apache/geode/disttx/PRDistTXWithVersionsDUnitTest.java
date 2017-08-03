1322deed8bb7c731122eea1f36f8a788aa1c6540
eshu <eshu@pivotal.io>
2017-08-03 10:56:53 -0700

    GEODE-3379: Geode transaction need to commit on primary buckets only.
    	Fail the transaction commit if primary buckets have been moved from transaction hosting node.
    
            (cherry picked from commit f7198be3228e333f65ad235ba06a0844ae258208)

--- geode-core/src/test/java/org/apache/geode/disttx/PRDistTXWithVersionsDUnitTest.java
+++ geode-core/src/test/java/org/apache/geode/disttx/PRDistTXWithVersionsDUnitTest.java
@@ -81,4 +81,8 @@ public class PRDistTXWithVersionsDUnitTest extends PRTransactionWithVersionsDUni
   @Test
   public void testBasicPRTransactionNoDataRedundancy2() {}
 
+  @Ignore("[DISTTX] TODO test overridden and intentionally left blank for distTx.")
+  @Override
+  @Test
+  public void testCommitToFailAfterPrimaryBucketMoved() {}
 }
